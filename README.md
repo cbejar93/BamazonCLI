13f057bb2a90923b65727852819c4d56ed74b0e8
unknown <cbejar93@gmail.com>
2018-04-30 21:28:35 -0400

    added a nice readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # BamazonCLI
+
+# Node.js & MySQL
+
+## Overview
+
+In this program users are able to choose between two tracks customer and managers.
+
+![ALT TEXT](assets/oneprompt.png) 
+
+Customers are able to look at a table produced by the console.table npm and then select the item_id 
+of the item they would like to purchase. Users also get a prompt to ask how much they would like to order.
+
+![ALT TEXT](assets/tablecust.png)
+
+![ALT TEXT](assets/custprompt.png)
+
+If there are enough to items to complete the sale, the amount the user requested is subtracted from the database. The new item amount is displayed and the user is notified that their order has been placed.
+
+![ALT TEXT](assets/order.png)
+
+The program terminates after that.
+
+If a user picks manager, they are presented with four different choices. 
+
+![ALT TEXT](assets/managerview.png)
+
+The first option gives the user a table of the items for sale like the one the customer recieves. 
+
+The secound option gives the user a table of any item with less than 3 units left in stock.
+
+![ALT TEXT](assets/lowinv.png)
+
+The third options allows the user to give an item more inventory.
+
+![ALT TEXT](assets/addinv.png)
+
+The last option allows the user to put an item of their choice into the databse.
+
+![ALT TEXT](assets/newprod.png)
+
+Here is a look at the databse table after all our changes. 
+
+![ALT TEXT](assets/database.png)
+
+This application uses node to be able to talk to a mySQL server.
+
+
+
+
+
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,9 +12,10 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+// This is the first connection to the database and gives the prompt to select user or manager 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Welcome to Amazon!")
+    console.log("Welcome to Bamazon!")
     inquirer.prompt([
         {
           name: "selection",
@@ -24,16 +25,14 @@ connection.connect(function(err) {
         }
       ]).then(function(res){
           if (res.selection === "Customer"){
-              console.log("hello");
               customer();
           }
           else{
               manager();
-              console.log("Amazon oppreses workers")
           }
       })  
   });
-
+// This function is called in two places and reads the database table 
   function readProducts() {
     console.log("Selecting all products...\n");
     connection.query("SELECT * FROM products", function(err, res) {
@@ -41,7 +40,7 @@ connection.connect(function(err) {
       console.table(res);  
         });
   }
-
+// One of two main function which allows users to select an item using ID and order the amount they wish.
   function customer(){
     readProducts();
       inquirer.prompt([
@@ -57,19 +56,23 @@ connection.connect(function(err) {
         }
          
     ]).then(function(response){
+        // This variable captures how many of an item the user would like to purchase 
           let customerquantity = response.quantity;
+        //   This get the id of the item the would like to purchase
           let customerChoice = response.id;
           connection.query("SELECT price price, stock_quanity FROM products WHERE ?", {item_id: customerChoice}, function(err,res){
             if (err) throw err;
+            // This grabs the amount of an item in stock from the database.
             let productQuantity =res[0].stock_quanity;
             console.log(customerquantity);
+            // This function takes the 3 variables to see if there are enough to order and to change the table to subtract the changes
             checker(customerquantity, productQuantity, customerChoice);
             
         })
          
       })
   }
-
+// The secound main function 
  function manager(){
     inquirer.prompt([
         {
@@ -79,25 +82,25 @@ connection.connect(function(err) {
           choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
         }
       ]).then(function(manres){
+        //   This grabs the users reply to the prompt above and sends them to function they want to run
           switch(manres.action){
               case "View Products for Sale":
                 readProducts();
                 break;
               case "View Low Inventory":
                 lowInv();
-                console.log("hello");
                 break;
               case "Add to Inventory":
                 addInv();
                 break; 
               case "Add New Product":
-                // newProd();
+                newProd();
                 console.log("triangles");
                 break;
           }
       })
  }
-
+// This is the function that checks if their is enough item in stock to order and then subtracts it from the table 
   function checker(x, y, z){
       let change = y-x;
       console.log(change)
@@ -124,14 +127,17 @@ connection.connect(function(err) {
 
     }
 
+    // This function grabs all the items that have less than 5 things in stock and displays it.
     function lowInv(){ 
         connection.query("SELECT * FROM products WHERE stock_quanity BETWEEN ? AND ?", [0, 5], function(err, respond){
             if (err) throw err;
             console.table(respond);
+            connection.end();
          
         })
     }
 
+    // This function allows a user to add more stock to an item 
     function addInv(){
         inquirer.prompt([
             {
@@ -145,11 +151,14 @@ connection.connect(function(err) {
                 message: "How much would you like to add?"
             }
         ]).then(function(response){
+            // grabs the id of the item they would like to update 
             let itemID = response.item;
+            // grabs how much they would like to add 
             let amountAdd = parseInt(response.adder);
             connection.query("SELECT * FROM products WHERE ?", {item_id: itemID}, function(err,res){
+                // grabs how much of the selected item is in stock 
                 let stockAmount = res[0].stock_quanity;
-                console.log(stockAmount);
+                // this function grabs the 3 local variables to add the amount to the table 
                 sqlAdder(amountAdd, stockAmount, itemID);
 
             })
@@ -157,7 +166,7 @@ connection.connect(function(err) {
         }
     )
     }
-
+// The function to add more to an item in the database
     function sqlAdder(x , y, z){
         connection.query(
             "UPDATE products SET ? WHERE ?",
@@ -171,9 +180,51 @@ connection.connect(function(err) {
             ], function(err){
                 if (err) throw err;
                 console.log("Action Complete!");
+                connection.end();
             }
         )
 
+    }
+// A function to add an new product to the database 
+    function newProd(){
+        inquirer.prompt([
+            {
+                name: "name",
+                message: "Please enter name of Product.",
+                type: "input"
+            },
+            {
+                name: "department",
+                message: "Please Select which Department",
+                type: "list",
+                choices: ["baby", "food", "beverages", "tech", "home"]
+            },
+            {
+                name: "cost",
+                message: "How much does the item cost?",
+                type: "input"
+            },
+            {
+                name: "amount",
+                message: "How much do we have in stock?",
+                type: "input"
+            }
+        ]).then(function(res){
+            console.log( res.name + " has been added to the database!")
+            connection.query(
+                "INSERT INTO products SET ?",
+                {
+                    product_name: res.name,
+                    department_name: res.department,
+                    price: res.cost,
+                    stock_quanity: res.amount
+                }, function(err){
+                    if (err) throw err;
+                    console.log("Action Complete!");
+                    connection.end();
+                }
+            )
+        })
     }
 
    
